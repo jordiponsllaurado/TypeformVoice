706293f27b5b15ee861933906b8e56f7bf229bbc
jordi.pons <jordi.pons@typeform.com>
2017-07-20 04:12:01 +0200

    Added voice into second question

--- Speech/app/src/main/java/com/google/cloud/android/speech/SecondQuestion.java
+++ Speech/app/src/main/java/com/google/cloud/android/speech/SecondQuestion.java
@@ -1,9 +1,21 @@
 package com.google.cloud.android.speech;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Locale;
@@ -15,11 +27,31 @@ import java.util.Locale;
 public class SecondQuestion extends AppCompatActivity {
 
     public TextToSpeech mTts;
+    private TextView mText;
+    private TextView mStatus;
+    private TextView mTextResult;
+
+
+    private SpeechService mSpeechService;
+
+    // Resource caches
+    private int mColorHearing;
+    private int mColorNotHearing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_question);
+
+        mTextResult = (TextView) findViewById(R.id.result);
+        mStatus = (TextView) findViewById(R.id.status);
+        final Resources resources = getResources();
+        final Resources.Theme theme = getTheme();
+        mColorHearing = ResourcesCompat.getColor(resources, R.color.status_hearing, theme);
+        mColorNotHearing = ResourcesCompat.getColor(resources, R.color.status_not_hearing, theme);
+        // Prepare Cloud Speech API
+        bindService(new Intent(this, SpeechService.class), mServiceConnection, BIND_AUTO_CREATE);
+        mText = (TextView) findViewById(R.id.welcome_text);
 
         mTts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
 
@@ -35,7 +67,7 @@ public class SecondQuestion extends AppCompatActivity {
 
                                 @Override
                                 public void run() {
-                                    //TODO
+                                    startVoiceRecorder();
                                 }
                             });
                         }
@@ -51,5 +83,104 @@ public class SecondQuestion extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //////////////// SPEECH API ////////////////
+    private VoiceRecorder mVoiceRecorder;
+    private final VoiceRecorder.Callback mVoiceCallback = new VoiceRecorder.Callback() {
+
+        @Override
+        public void onVoiceStart() {
+            showStatus(true);
+            if (mSpeechService != null) {
+                mSpeechService.startRecognizing(mVoiceRecorder.getSampleRate());
+            }
+        }
+
+        @Override
+        public void onVoice(byte[] data, int size) {
+            if (mSpeechService != null) {
+                mSpeechService.recognize(data, size);
+            }
+        }
+
+        @Override
+        public void onVoiceEnd() {
+            showStatus(false);
+            if (mSpeechService != null) {
+                mSpeechService.finishRecognizing();
+            }
+        }
+
+    };
+
+    private void showStatus(final boolean hearingVoice) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mStatus.setTextColor(hearingVoice ? mColorHearing : mColorNotHearing);
+            }
+        });
+    }
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder binder) {
+            mSpeechService = SpeechService.from(binder);
+            mSpeechService.addListener(mSpeechServiceListener);
+            mStatus.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mSpeechService = null;
+        }
+
+    };
+
+    private final SpeechService.Listener mSpeechServiceListener =
+            new SpeechService.Listener() {
+                @Override
+                public void onSpeechRecognized(final String text, final boolean isFinal) {
+                    if (isFinal) {
+                        mVoiceRecorder.dismiss();
+                    }
+                    if (mText != null && !TextUtils.isEmpty(text)) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mTextResult.setText(text);
+
+                                if (isFinal) {
+                                    mTextResult.setText(text);
+                                    mVoiceRecorder.stop();
+                                    Intent intent = new Intent(getApplicationContext(), WhatTitle.class);
+                                    startActivity(intent);
+                                } else {
+                                    mTextResult.setText(text);
+                                }
+                            }
+                        });
+                    }
+                }
+            };
+
+    private void startVoiceRecorder() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (mVoiceRecorder != null) {
+                mVoiceRecorder.stop();
+            }
+            mVoiceRecorder = new VoiceRecorder(mVoiceCallback);
+            mVoiceRecorder.start();
+        }
+    }
+
+    private void stopVoiceRecorder() {
+        if (mVoiceRecorder != null) {
+            mVoiceRecorder.stop();
+            mVoiceRecorder = null;
+        }
     }
 }
