69128c41fb353b50d64ed6d3b04c39fb68d2da57
jordi.pons <jordi.pons@typeform.com>
2017-07-20 08:26:47 +0200

    Added Camera

--- Speech/app/src/main/java/com/google/cloud/android/speech/Camera.java
+++ Speech/app/src/main/java/com/google/cloud/android/speech/Camera.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
+import android.provider.MediaStore;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.res.ResourcesCompat;
@@ -15,8 +18,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 
@@ -24,13 +32,15 @@ import java.util.Locale;
  * Created by jordipons on 20/07/2017.
  */
 
-public class ThankYou extends AppCompatActivity {
+public class Camera extends AppCompatActivity {
 
     public TextToSpeech mTts;
     private TextView mText;
     private TextView mStatus;
     private TextView mTextResult;
 
+    private static final int CAMERA_PIC_REQUEST = 1337;
+    private String mCurrentPhotoPath;
 
     private SpeechService mSpeechService;
 
@@ -38,10 +48,13 @@ public class ThankYou extends AppCompatActivity {
     private int mColorHearing;
     private int mColorNotHearing;
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.thank_you);
+        setContentView(R.layout.camera);
 
         mTextResult = (TextView) findViewById(R.id.result);
         mStatus = (TextView) findViewById(R.id.status);
@@ -50,8 +63,7 @@ public class ThankYou extends AppCompatActivity {
         mColorHearing = ResourcesCompat.getColor(resources, R.color.status_hearing, theme);
         mColorNotHearing = ResourcesCompat.getColor(resources, R.color.status_not_hearing, theme);
         // Prepare Cloud Speech API
-        bindService(new Intent(this, SpeechService.class), mServiceConnection, BIND_AUTO_CREATE);
-        mText = (TextView) findViewById(R.id.welcome_text);
+        //bindService(new Intent(this, SpeechService.class), mServiceConnection, BIND_AUTO_CREATE);
 
         mTts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
 
@@ -67,7 +79,7 @@ public class ThankYou extends AppCompatActivity {
 
                                 @Override
                                 public void run() {
-                                    startVoiceRecorder();
+                                    dispatchTakePictureIntent();
                                 }
                             });
                         }
@@ -76,7 +88,7 @@ public class ThankYou extends AppCompatActivity {
                     HashMap<String, String> params = new HashMap<String, String>();
 
                     params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID,"stringId");
-                    mTts.speak("Last block would be Thank you for participating as a thank you screen.\n Do you want to accept, rephrase, add a picture or remove the block?", TextToSpeech.QUEUE_FLUSH, params);
+                    mTts.speak("camera", TextToSpeech.QUEUE_FLUSH, params);
                 } else {
                     mTts = null;
                     Log.e("MainActivity", "Failed to initialize the TextToSpeech engine");
@@ -168,8 +180,8 @@ public class ThankYou extends AppCompatActivity {
                                 if (isFinal) {
                                     mTextResult.setText(text);
                                     mVoiceRecorder.stop();
-                                    Intent intent = new Intent(getApplicationContext(), Camera.class);
-                                    startActivity(intent);
+                                    //Intent intent = new Intent(getApplicationContext(), Camera.class);
+                                    //startActivity(intent);
                                 } else {
                                     mTextResult.setText(text);
                                 }
@@ -195,5 +207,95 @@ public class ThankYou extends AppCompatActivity {
             mVoiceRecorder.stop();
             mVoiceRecorder = null;
         }
+    }
+
+    //////// CAMERA ////////
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageview = (ImageView) findViewById(R.id.ImageView01); //sets imageview as the bitmap
+            imageview.setImageBitmap(imageBitmap);
+
+            mTts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+
+                @Override
+                public void onInit(int i) {
+                    if (i == TextToSpeech.SUCCESS) {
+                        mTts.setOnUtteranceCompletedListener(new TextToSpeech.OnUtteranceCompletedListener() {
+
+                            @Override
+                            public void onUtteranceCompleted(String utteranceId) {
+
+                                runOnUiThread(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        startVoiceRecorder();
+                                    }
+                                });
+                            }
+                        });
+                        mTts.setLanguage(Locale.getDefault());
+                        HashMap<String, String> params = new HashMap<String, String>();
+
+                        params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID,"stringId");
+                        mTts.speak("The picture has added to your block.\n Do you want to change anything else?", TextToSpeech.QUEUE_FLUSH, params);
+                    } else {
+                        mTts = null;
+                        Log.e("MainActivity", "Failed to initialize the TextToSpeech engine");
+                    }
+                }
+            });
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    /*private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                //TODO
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.google.cloud.android.speech",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }*/
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
